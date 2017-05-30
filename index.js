a7f8e416afb0d2d89e461dfdc53e2376394f782d
jorge <jorge.valdemar.s@gmail.com>
2017-05-29 21:00:33 -0300

    teste

--- index.js
+++ index.js
@@ -2,8 +2,15 @@
 var express = require('express');
 var app = express();
 var expressWs = require('express-ws')(app);
-var server = app.listen(process.env.PORT || 3000, "0.0.0.0");
+//var server = app.listen(process.env.PORT || 3000);
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+ 
+var server = app.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port );
+});
 
 require('./routes')(app, expressWs);
 
-console.log('Servidor iniciado na porta' + server.address().port);
+//console.log('Servidor iniciado na porta' + server.address().port);
